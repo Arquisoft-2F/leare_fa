1b7a3d84fb70d9f15ec9f0243280f90d8ab6ed70
Camilo-neck <ccuello@unal.edu.co>
2024-04-11 22:03:03 -0500

    si

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
                 secondary: const Color.fromARGB(255, 98, 204, 252)),
             useMaterial3: true,
           ),
-          home: const SectionPage(),
+          home: const LandingPage(),
           routes: {
             '/login': (context) => const LoginPage(),
             '/register': (context) => const RegisterPage(),
