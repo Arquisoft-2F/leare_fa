0d840fd666ea82959f98f2a4358cd85ee1a2abfb
jsarmientopu <jsarmientopu@unal.edu.co>
2024-05-21 18:51:33 -0500

    cambio de nuevo

--- lib/graphql_config.dart
+++ lib/graphql_config.dart
@@ -11,7 +11,7 @@ class GraphQlConfiguration {
         (X509Certificate cert, String host, int port) => true;
     IOClient _ioClient = new IOClient(_httpClient);
     final HttpLink httpLink = HttpLink(
-      'http://leare-gateway:5555/graphql',
+      'https://movil-proxy/graphql',
       defaultHeaders: {
         'Authorization': 'Bearer ${token ?? 'your_default_token_here'}',
         'Access-Control-Allow-Origin': '*'
--- lib/utils/upload_file.dart
+++ lib/utils/upload_file.dart
@@ -40,7 +40,7 @@ Future<String> uploadFile({
   try {
     var request = http.MultipartRequest(
       'POST',
-      Uri.parse('http://ws:8001/documents/upload'),
+      Uri.parse('https://movil-proxy/documents/upload'),
     );
 
     request.fields['file_name'] = file_name;
